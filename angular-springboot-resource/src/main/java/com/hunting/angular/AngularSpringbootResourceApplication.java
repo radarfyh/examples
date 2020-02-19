5f21390b478746e10559d8d25a0f14428bf468c5
radarfyh <radarfyh@gamil.com>
2020-02-19 21:47:16 +0800

    新增配置项：redis数据库索引 修改其他bug。

--- angular-springboot-resource/src/main/java/com/hunting/angular/AngularSpringbootResourceApplication.java
+++ angular-springboot-resource/src/main/java/com/hunting/angular/AngularSpringbootResourceApplication.java
@@ -7,23 +7,34 @@ import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @RestController
-public class AngularSpringbootResourceApplication {
-	@RequestMapping("/")
-	@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = { "x-auth-token", "x-requested-with", "x-xsrf-token" })
-	public Message home() {
-		return new Message("这是一次严格的测试！");
-	}
+public class AngularSpringbootResourceApplication extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors()
+      .and()
+        .authorizeRequests()
+          .anyRequest().authenticated();
+  }
 
-	@Bean
-	HeaderHttpSessionStrategy sessionStrategy() {
-		return new HeaderHttpSessionStrategy();
-	}
+  @RequestMapping("/")
+  @CrossOrigin(origins = "*", maxAge = 3600,
+      allowedHeaders = {"x-auth-token", "x-requested-with", "x-xsrf-token"})
+  public Message home() {
+    return new Message("这是一次严肃的测试。");
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(AngularSpringbootResourceApplication.class, args);
-	}
+  @Bean
+  HeaderHttpSessionStrategy sessionStrategy() {
+    return new HeaderHttpSessionStrategy();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(AngularSpringbootResourceApplication.class, args);
+  }
 
 }
